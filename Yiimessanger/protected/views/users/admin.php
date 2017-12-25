9cfdce4b18d5684a1815efb3c2618e04b76c443b
3axhax <3axhax@gmail.com>
2017-12-25 16:52:22 +0500

    1

--- Yiimessanger/protected/views/users/admin.php
+++ Yiimessanger/protected/views/users/admin.php
@@ -53,7 +53,7 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 		//'online',
 		array(
 			'name' => 'online',
-			'value' => $data,
+			'value' => '$data->online ? online : offline',
 		),
 		array(
 			'class'=>'CButtonColumn',
